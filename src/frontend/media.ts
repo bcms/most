d6e39f84c2d30e79b31d25a27f5f0cf1866481aa
Bbanez <banezprod@gmail.com>
2023-07-05 13:38:46 +0200

    check if local processing is active

--- src/frontend/media.ts
+++ src/frontend/media.ts
@@ -2,5 +2,8 @@ import type { BCMSMediaParsed } from '@becomes/cms-client/types';
 import { BCMSImageConfig } from './image-config';
 
 export function bcmsMediaToUrl(media: BCMSMediaParsed): string {
+  if (BCMSImageConfig.localeImageProcessing) {
+    return `/bcms-media${media.src}`;
+  }
   return `${BCMSImageConfig.cmsOrigin}/api/media/pip/${media._id}/bin/${BCMSImageConfig.publicApiKeyId}/${media.name}`;
 }
